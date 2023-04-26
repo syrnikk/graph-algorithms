d25dea6f93818d01c0d86c29b72f232db6c2d369
ptakpiotr <piter020@outlook.com>
2023-04-26 22:12:05 +0200

    Fix for reading and writing matrix

--- lab1/graph_reader_writer.py
+++ lab1/graph_reader_writer.py
@@ -23,7 +23,7 @@ def read_list(src, v_sep=":", n_sep=","):
             values = list(map(lambda x: int(x), [h[1], *d[1:]]))
             result.append((int(h[0]), values))
         else:
-            result.append((int(h[0]),[]))
+            result.append((int(h[0]), []))
 
     return dict(result)
 
@@ -62,15 +62,18 @@ def read_incidence_matrix(src, no_v=False, no_e=False, v_sep=" ", e_sep=" ", a_s
 
     matrix = np.matrix(matrix, dtype=int)
 
+    if len(edges) != matrix.shape[1] and not no_e:
+        raise ValueError('Incorrect matrix')
+
     return (vertices, edges, matrix)
 
 
-def read_nei_matrix(src, no_v=False, v_sep=" ", n_sep=","):
+def read_nei_matrix(src, v=False, v_sep=" ", n_sep=","):
     """Function that reads graph represented as neighbor matrix
 
     Args:
         src (string): string value of graph represented as neighbor matrix 
-        no_v (bool, optional): no vertices labels. Defaults to False.
+        v (bool, optional): vertices labels. Defaults to False.
         v_sep (str, optional): vertices separator. Defaults to " ".
         n_sep (str, optional): neighbors separator. Defaults to ",".
 
@@ -79,13 +82,20 @@ def read_nei_matrix(src, no_v=False, v_sep=" ", n_sep=","):
     """
 
     data = list(map(lambda x: x[:-1], src))
+    vertices = [i+1 for i in range(len(data))] if not v else []
+    matrix = list(map(lambda x: x.split(n_sep), data))
 
-    vertices = [str(i+1) for i in range(len(data))
-                ] if no_v else data[0].split(v_sep)
+    for i, r in enumerate(matrix):
+        if v:
+            first_cell = r[0].split(v_sep)
+            vertices.append(first_cell[0])
+            matrix[i][0] = first_cell[1]
 
-    matrix = list(map(lambda x: x.split(n_sep), data[0 if no_v else 1:]))
+    if len(vertices) != len(matrix[0]):
+        raise ValueError('Incorrect matrix shape')
 
     matrix = np.matrix(matrix, dtype=int)
+
     return (vertices, matrix)
 
 
@@ -142,6 +152,7 @@ def print_incidence_matrix(vertices, edges, matrix, sep=" "):
     shape = np.shape(matrix)
 
     print(f'{sep}{sep.join([str(e) for e in edges])}')
+
     for i in range(shape[0]):
         if len(vertices) > 0:
             print(vertices[i], end=sep)
@@ -155,10 +166,25 @@ if __name__ == "__main__":
         li = read_list(f, v_sep=";", n_sep=",")
         print_list(li)
         print()
+    with open("./examples/list_2.txt", "r") as f:
+        li = read_list(f, v_sep="-", n_sep="n")
+        print_list(li)
+        print()
     with open("./examples/nei_matrix.txt", "r") as f:
-        mt = read_nei_matrix(f.readlines(), v_sep=" ", n_sep=" ")
+        mt = read_nei_matrix(f, v=False, v_sep=" ", n_sep=" ")
         print_nei_matrix(mt[0], mt[1])
         print()
+    with open("./examples/nei_matrix_2.txt", "r") as f:
+        mt = read_nei_matrix(f, v=True, v_sep="t", n_sep=" ")
+        print_nei_matrix(mt[0], mt[1])
+        print()
+    try:
+        with open("./examples/nei_matrix_3.txt", "r") as f:
+            mt = read_nei_matrix(f, v=False, n_sep="u")
+            print_nei_matrix(mt[0], mt[1])
+            print()
+    except ValueError as e:
+        print(e)
     with open("./examples/inc_matrix.txt", "r") as f:
         adj = read_incidence_matrix(f, v_sep=";", e_sep="f", a_sep=" ")
         print_incidence_matrix(adj[0], adj[1], adj[2])
@@ -167,7 +193,10 @@ if __name__ == "__main__":
         adj = read_incidence_matrix(f, no_e=True, v_sep="x", a_sep=" ")
         print_incidence_matrix(adj[0], adj[1], adj[2])
         print()
-    with open("./examples/inc_matrix_no_edges_vertices.txt", "r") as f:
-        adj = read_incidence_matrix(f, no_e=True, no_v=True, v_sep="x", a_sep="t")
-        print_incidence_matrix(adj[0], adj[1], adj[2])
-        print()
+    try:
+        with open("./examples/inc_matrix_2.txt", "r") as f:
+            adj = read_incidence_matrix(f, v_sep=";", e_sep="f", a_sep=" ")
+            print_incidence_matrix(adj[0], adj[1], adj[2])
+            print()
+    except ValueError as e:
+        print(e)
